6e92a4dfb091630b48fa45e9891f95f4f41d94c8
Kesedy <kosimjon.kosimov>
2023-04-24 11:49:42 +0500

    #6 Dynamic Routing (Set up dynamic routing)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>A site for events to better the world.</h1>
+    <h1>
+      A car is a motorized trackless road and / or off-road, most often autonomous, vehicle used to
+      transport people and / or goods, having four or more wheels. The main purpose of the car is to
+      carry out transport work.
+    </h1>
   </div>
 </template>
+
+<style>
+.about {
+  color: #696b6e;
+  margin: 0 auto;
+  max-width: 1200px;
+  font-size: 12px;
+  text-align: left;
+}
+</style>
